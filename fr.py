0c857160d4123cb8c6bf0da3cdebffea63bd9aff
bbbbh11 <90540968+bbbbh11@users.noreply.github.com>
2021-09-19 22:58:58 +0800

    Update fr.py

--- fr.py
+++ fr.py
@@ -1,15 +1,4 @@
 import math as m
-def g(n):
-    #返回n的小数位数，去掉末尾的0
-    if(int(n)==n):
-        return(0)
-    n=str(n)
-    while(n[-1]=='0'):
-        n=n[0:-1]
-    s=len(n)
-    for i in range(s):
-        if(n[i]=='.'):
-            return(s-i-1)
 class frac(object):
     def __init__(frac,a,b):
         if(b==0):
@@ -18,10 +7,16 @@ class frac(object):
             frac.a=0
             frac.b=1
             return None
-        s=10**max(g(a),g(b))
-        a=int(a*s)
-        b=int(b*s)
-        s=m.gcd(a,b)
+        s=0
+        while(a!=int(a)):
+            s+=1
+            a*=10
+        b=b*10**s
+        while(b!=int(b)):
+            s+=1
+            a*=10
+            b*=10
+        s=m.gcd(int(a),int(b))
         a/=s
         b/=s
         if(b<0):
@@ -32,26 +27,18 @@ class frac(object):
     def __add__(frac1,frac2):
         return(frac(frac1.a*frac2.b+frac1.b*frac2.a,frac1.b*frac2.b))
     def __repr__(frac):
-        return("%d/%d"%(frac.a,frac.b))
-def tofrac(n):
-    if(type(n)==frac):
-        return(n)
-    return(frac(n,1))
+        return('%d/%d'%(frac.a,frac.b))
 class fr(object):
-    def __init__(fr,a,b,c,skip=False):
-        #fr(a,b,c)表示a+b/c,一般地,程序会自动化简;若skip为True,则跳过化简环节
+    def __init__(fr,a,b,c):
+        #fr(a,b,c)表示a+b/c,一般地,程序会自动化简
         #目前，我们支持取绝对值，四则运算，取余，转化为浮点数或整数，比较运算
-        if(skip):
-            fr.a,fr.b,fr.c=a,b,c
-            return(None)
         a的整数部分=int(a)
-        a的小数位数=g(a)
-        a的小数部分=int((a-a的整数部分)*10**a的小数位数)/10**a的小数位数
-        分数部分=frac(a的小数部分,1)+frac(b,c)
-        a=int(分数部分.a/分数部分.b)
-        c=分数部分.b
-        b=分数部分.a-a*c
-        a+=a的整数部分
+        a=frac(a-a的整数部分,1)
+        a+=frac(b,c)
+        s=int(a.a/a.b)
+        a的整数部分+=s
+        a.a-=s*a.b
+        a,b,c=a的整数部分,a.a,a.b
         if(a<0 and b>0):
             a+=1
             b-=c
@@ -59,39 +46,33 @@ class fr(object):
             a-=1
             b+=c
         fr.a,fr.b,fr.c=a,b,c
-    def __repr__(fr):
-        return('fr(%d,%d,%d)'%(fr.a,fr.b,fr.c))
+    def __str__(fr):
+        if fr.b==0:
+            return str(fr.a)
+        if(fr.a==0):
+            return "%d/%d"%(fr.b,fr.c)
+        if(fr.a>0):
+            return("%d+%d/%d"%(fr.a,fr.b,fr.c))
+        return('%d%d/%d',fr.a,fr.b,fr.c)
     def __add__(fr1,fr2):
         if(type(fr1)!=fr):
-            fr1=tofrac(fr1)
-            s=int(fr1.a/fr1.b)
-            fr1=fr(s,fr1.a-s*fr1.b,fr1.b,True)
+            fr1=fr(0,fr1,1)
         if(type(fr2)!=fr):
-            fr2=tofrac(fr2)
-            s=int(fr2.a/fr2.b)
-            fr2=fr(s,fr2.a-s*fr2.b,fr2.b,True)
+            fr2=fr(0,fr2,1)
         return(fr(fr1.a+fr2.a,fr1.b*fr2.c+fr1.c*fr2.b,fr1.c*fr2.c))
     def __eq__(fr1,fr2):
         if(type(fr1)!=fr):
-            fr1=tofrac(fr1)
-            s=int(fr1.a/fr1.b)
-            fr1=fr(s,fr1.a-s*fr1.b,fr1.b,True)
+            fr1=fr(0,fr1,1)
         if(type(fr2)!=fr):
-            fr2=tofrac(fr2)
-            s=int(fr2.a/fr2.b)
-            fr2=fr(s,fr2.a-s*fr2.b,fr2.b,True)
+            fr2=fr(0,fr2,1)
         if(fr1.a!=fr2.a or fr1.b!=fr2.b or fr1.c!=fr2.c):
             return False
         return True
     def __lt__(fr1,fr2):
         if(type(fr1)!=fr):
-            fr1=tofrac(fr1)
-            s=int(fr1.a/fr1.b)
-            fr1=fr(s,fr1.a-s*fr1.b,fr1.b,True)
+            fr1=fr(0,fr1,1)
         if(type(fr2)!=fr):
-            fr2=tofrac(fr2)
-            s=int(fr2.a/fr2.b)
-            fr2=fr(s,fr2.a-s*fr2.b,fr2.b,True)
+            fr2=fr(0,fr2,1)
         if(fr1.a<fr2.a):
             return True
         elif(fr1.b*fr2.c<fr2.b*fr1.c):
@@ -99,13 +80,9 @@ class fr(object):
         return False
     def __gt__(fr1,fr2):
         if(type(fr1)!=fr):
-            fr1=tofrac(fr1)
-            s=int(fr1.a/fr1.b)
-            fr1=fr(s,fr1.a-s*fr1.b,fr1.b,True)
+            fr1=fr(0,fr1,1)
         if(type(fr2)!=fr):
-            fr2=tofrac(fr2)
-            s=int(fr2.a/fr2.b)
-            fr2=fr(s,fr2.a-s*fr2.b,fr2.b,True)
+            fr2=fr(0,fr2,1)
         if(fr1.a>fr2.a):
             return True
         elif(fr1.b*fr2.c>fr2.b*fr1.c):
@@ -113,13 +90,9 @@ class fr(object):
         return False
     def __ge__(fr1,fr2):
         if(type(fr1)!=fr):
-            fr1=tofrac(fr1)
-            s=int(fr1.a/fr1.b)
-            fr1=fr(s,fr1.a-s*fr1.b,fr1.b,True)
+            fr1=fr(0,fr1,1)
         if(type(fr2)!=fr):
-            fr2=tofrac(fr2)
-            s=int(fr2.a/fr2.b)
-            fr2=fr(s,fr2.a-s*fr2.b,fr2.b,True)
+            fr2=fr(0,fr2,1)
         if(fr1.a>=fr2.a):
             return True
         elif(fr1.b*fr2.c>=fr2.b*fr1.c):
@@ -127,13 +100,9 @@ class fr(object):
         return False
     def __le__(fr1,fr2):
         if(type(fr1)!=fr):
-            fr1=tofrac(fr1)
-            s=int(fr1.a/fr1.b)
-            fr1=fr(s,fr1.a-s*fr1.b,fr1.b,True)
+            fr1=fr(0,fr1,1)
         if(type(fr2)!=fr):
-            fr2=tofrac(fr2)
-            s=int(fr2.a/fr2.b)
-            fr2=fr(s,fr2.a-s*fr2.b,fr2.b,True)
+            fr2=fr(0,fr2,1)
         if(fr1.a<=fr2.a):
             return True
         elif fr1.b*fr2.c<=fr2.b*fr1.c:
@@ -149,43 +118,40 @@ class fr(object):
         return fr
     def __sub__(fr1,fr2):
         if(type(fr1)!=fr):
-            fr1=tofrac(fr1)
-            s=int(fr1.a/fr1.b)
-            fr1=fr(s,fr1.a-s*fr1.b,fr1.b,True)
+            fr1=fr(0,fr1,1)
         if(type(fr2)!=fr):
-            fr2=tofrac(fr2)
-            s=int(fr2.a/fr2.b)
-            fr2=fr(s,fr2.a-s*fr2.b,fr2.b,True)
+            fr2=fr(0,fr2,1)
         return(fr(fr1.a-fr2.a,fr1.b*fr2.c-fr2.b*fr1.c,fr1.c*fr2.c))
     def __mul__(fr1,fr2):
         if(type(fr1)!=fr):
-            fr1=tofrac(fr1)
-            s=int(fr1.a/fr1.b)
-            fr1=fr(s,fr1.a-s*fr1.b,fr1.b,True)
+            fr1=fr(0,fr1,1)
         if(type(fr2)!=fr):
-            fr2=tofrac(fr2)
-            s=int(fr2.a/fr2.b)
-            fr2=fr(s,fr2.a-s*fr2.b,fr2.b,True)
+            fr2=fr(0,fr2,1)
         return(fr(fr1.a*fr2.b,fr2.b*fr1.c*(fr1.a+1)+fr1.b*fr2.c*(fr2.a+1),fr1.c*fr2.c))
     def __truediv__(fr1,fr2):
         if(type(fr1)!=fr):
-            fr1=tofrac(fr1)
-            s=int(fr1.a/fr1.b)
-            fr1=fr(s,fr1.a-s*fr1.b,fr1.b,True)
+            fr1=fr(0,fr1,1)
         if(type(fr2)!=fr):
-            fr2=tofrac(fr2)
-            s=int(fr2.a/fr2.b)
-            fr2=fr(s,fr2.a-s*fr2.b,fr2.b,True)
+            fr2=fr(0,fr2,1)
         return fr(0,fr2.c*(fr1.a*fr1.c+fr1.b),fr1.c*(fr2.a*fr2.c+fr2.b))
     def __floordiv__(fr1,fr2):
         return(int(fr1/fr2))
     def __mod__(fr1,fr2):
         return(fr1-fr2*int(fr1/fr2))
 #这里提供一个将其他类型的数转化为fr类的方法:)
-def tofr(n):
+def tofr(n) -> any:
     s=type(n)
+    if(s==str):
+        try:
+            n=float(n)
+        except:
+            def lf(m):
+                if(m!=''):
+                    return float(m)
+                return 0
+            n=n.split('+')
+            n[1]=n[1].split('/')
+            return fr(lf(n[0]),lf(n[1][0]),lf(n[1][1]))
     if s==fr:
         return n
-    elif s==frac:
-        return fr(0,n.a,n.b)
     return fr(0,n,1)
